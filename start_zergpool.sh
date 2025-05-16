97b8e4a9da5445910741f3278e01cd688361c97b
Leandro Pereira Costa <leandro.p.c.86@gmail.com>
2025-05-16 10:51:04 -0300

    Update start_zergpool.sh

--- start_zergpool.sh
+++ start_zergpool.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER "
-./SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER 
+/opt/SRBMiner-Multi-2-8-8/SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER 
