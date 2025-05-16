e0970d69abb0f2edb856e3ddab8951678c07cdaf
Leandro Pereira Costa <leandro.p.c.86@gmail.com>
2025-05-16 09:47:58 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 version="2.4.7"
-image="srbminer-multi-cuda"
-docker build . --tag docker.io/cniweb/$image:$version
-docker tag docker.io/cniweb/$image:$version docker.io/cniweb/$image:latest
-docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:$version
-docker tag docker.io/cniweb/$image:$version ghcr.io/cniweb/$image:latest
-docker tag docker.io/cniweb/$image:$version quay.io/cniweb/$image:$version
-docker tag docker.io/cniweb/$image:$version quay.io/cniweb/$image:latest
+image="srbminer-multi-cuda-le"
+docker build . --tag docker.io/lpc86/$image:$version
+docker tag docker.io/lpc86/$image:$version docker.io/cniweb/$image:latest
 docker push docker.io/cniweb/$image:$version
 docker push docker.io/cniweb/$image:latest
-docker push ghcr.io/cniweb/$image:$version
-docker push ghcr.io/cniweb/$image:latest
-docker push quay.io/cniweb/$image:$version
-docker push quay.io/cniweb/$image:latest
