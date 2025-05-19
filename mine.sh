76c139739f14910dca18507baaafb32ce6f281c8
Leandro Pereira Costa <leandro.p.c.86@gmail.com>
2025-05-19 20:57:45 -0300

    Update and rename start_zergpool.sh to mine.sh

---
+++ mine.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Start..."
+/opt/SRBMiner-MULTI --algorithm randomx --pool mine-tari-monero.luckypool.io:8118 --wallet 12D1CtJnkqS4CMCFY2No5xeUWpzjH1rZkGHyZoHqYV14aE9mLLVyeuZj1siCh4gsMJgVZKa5aCraZGTEjB5n3oQjm2Y+47XpnLL3raj7aTCAdXFhHZUcNhumxcxfc2mvwVmFfL6udAGzuhZQExpSVyJj3yPbgLDoudJg2acaZdHdfziAbBB81RDzo89.salad
+echo "End???"
+sleep 31000000
--- start_zergpool.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Start SRBMiner-MULTI with Parameters: --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER "
-/opt/SRBMiner-Multi-2-8-8/SRBMiner-MULTI --algorithm $ALGO --pool $POOL_ADDRESS --wallet $WALLET_USER 
